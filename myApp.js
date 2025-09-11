439f7895b87cd79c4048e61343cb33d85be57cc4
RojoBlack <143372849+RojoBlack@users.noreply.github.com>
2025-09-11 12:44:48 -0300

    parámetros de consulta

--- myApp.js
+++ myApp.js
@@ -20,6 +20,12 @@ app.get("/:word/echo", (req, res) => {
   res.json({ echo: req.params.word });
 });
 
+app.get("/name", (req, res) => {
+  const firstName = req.query.first;
+  const lastName = req.query.last;
+  res.json({ name: `${firstName} ${lastName}` });
+});
+
 
 // ✅ servir archivos estáticos desde /public
 app.use("/public", express.static(__dirname + "/public"));
